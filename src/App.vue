acf4c856a3a6d426fafe74be5997bf4b39430408
rawjiamo <yangwei_dmu@126.com>
2022-04-07 21:48:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="hello">
+      <span>Hello Vite + Vue2</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+    background-color: #999;
+  }
+  .hello {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 200px;
+    color: #fff;
+  }
+</style>
